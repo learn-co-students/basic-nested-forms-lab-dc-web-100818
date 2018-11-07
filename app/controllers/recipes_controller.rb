94b2f524232e475c6a1e5347fce4fef3d112c1f3
jennyjean <jennyjean8675309@yahoo.com>
2018-11-07 11:41:05 -0500

    Used nested forms to create a new recipe instance that has_many ingredientsj

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,26 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+
+    @recipe.ingredients.build(quantity: nil)
+    @recipe.ingredients.build(quantity: nil)
   end
 
   def create
+    recipe = Recipe.create(recipe_strong_params)
+
+    redirect_to recipe_path(recipe)
+  end
+
+  private
+
+  def recipe_strong_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
   end
 end
